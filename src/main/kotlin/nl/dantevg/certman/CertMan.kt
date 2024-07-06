b08c0d9b693a241f18035b67a596f7428e36370f
Dantevg <dantevg006@gmail.com>
2024-07-06 15:59:03 +0200

    Make CertMan plugin an object, not a class

--- src/main/kotlin/nl/dantevg/certman/CertMan.kt
+++ src/main/kotlin/nl/dantevg/certman/CertMan.kt
@@ -2,7 +2,7 @@ package nl.dantevg.certman
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class CertMan : JavaPlugin() {
+object CertMan : JavaPlugin() {
 	override fun onEnable() {
 		dataFolder.mkdirs()
 		saveDefaultConfig()
